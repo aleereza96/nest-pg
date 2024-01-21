ca345f09f67bf003fa78ca336f1d741f43f9246b
aleereza mousavi <aleereza96m@gmail.com>
2024-01-21 23:29:46 +0330

    changed main.ts and used config variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const configService = app.get(ConfigService)
+
+  app.setGlobalPrefix(configService.get('app.apiPrefix'))
+
+  const allowedOrigins =
+    configService.get('app.allowedOrigins').split(',') || []
+  app.enableCors({ origin: allowedOrigins })
+
+  const port = configService.get('app.port')
+  await app.listen(port)
 }
 bootstrap()
