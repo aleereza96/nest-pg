b66e0c3f91cb71d8ca2d78ebe7804a465b2c6f88
aleereza mousavi <aleereza96m@gmail.com>
2024-08-17 08:31:18 +0330

    users module and dto

--- src/app/modules/user/dto/create-user.dto.ts
+++ src/app/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator"
+
+
+export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'username of the user',
+  })
+  @IsString()
+  @IsNotEmpty()
+  username: string
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'password of the user',
+  })
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string
+}
